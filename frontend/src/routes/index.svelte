43d7bf54e729d0175def69794546b2db1811a3c0
billy4479 <giachi.ellero@gmail.com>
2021-11-30 17:47:04 -0330

    Frontend: fix selection styles

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,34 +1,33 @@
 <script lang="ts">
   import { isLoggedIn, isAuthenticatedStore } from '../lib/api/login';
   import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
 
-  const p = isLoggedIn().then(async (loggedIn) => {
-    if (loggedIn) {
-      await goto('/a');
-    }
-  });
+  onMount(() =>
+    isLoggedIn().then(async (loggedIn) => {
+      if (loggedIn) {
+        await goto('/a');
+      }
+    })
+  );
 </script>
 
-{#await p}
-  Loading...
-{:then}
-  {#if !$isAuthenticatedStore}
-    <h1 class="text-center text-6xl mt-10">Landing page</h1>
+{#if !$isAuthenticatedStore}
+  <h1 class="text-center text-6xl mt-10">Landing page</h1>
 
-    <div class="flex justify-center place-items-center flex-col">
-      <p class="mt-10 w-2/3 text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nam
-        quod repudiandae consequuntur natus quo minus vitae voluptas temporibus
-        itaque voluptate assumenda ratione, quibusdam quam cumque dolorem nobis
-        aliquid omnis adipisci tempore veniam at? Officiis ad, delectus,
-        accusantium nam corporis voluptates iusto saepe molestiae velit ipsum
-        modi? Corporis, amet asperiores?
-      </p>
+  <div class="flex justify-center place-items-center flex-col">
+    <p class="mt-10 w-2/3 text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nam
+      quod repudiandae consequuntur natus quo minus vitae voluptas temporibus
+      itaque voluptate assumenda ratione, quibusdam quam cumque dolorem nobis
+      aliquid omnis adipisci tempore veniam at? Officiis ad, delectus,
+      accusantium nam corporis voluptates iusto saepe molestiae velit ipsum
+      modi? Corporis, amet asperiores?
+    </p>
 
-      <div class="mt-10">
-        <a href="/login" class="btn-good-light">Login</a>
-        <a href="/register" class="btn-good">Register</a>
-      </div>
+    <div class="mt-10">
+      <a href="/login" class="btn-good-light">Login</a>
+      <a href="/register" class="btn-good">Register</a>
     </div>
-  {/if}
-{/await}
+  </div>
+{/if}
