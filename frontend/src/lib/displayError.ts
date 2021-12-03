a3e8514c85304a8baafac0949b8a9065d4195d1a
billy4479 <giachi.ellero@gmail.com>
2021-12-02 22:03:00 -0330

    Frontend: Create folder and upload from right click menu

--- frontend/src/lib/displayError.ts
+++ frontend/src/lib/displayError.ts
@@ -2,7 +2,8 @@ import { writable } from 'svelte/store';
 
 export const errorStore = writable<string>('');
 
-export function displayError(message: string) {
+export function displayError(message: string): void {
   console.error(message);
   errorStore.set(message);
+  debugger;
 }
--- frontend/src/routes/a/folder/[id].svelte
+++ frontend/src/routes/a/folder/[id].svelte
@@ -5,36 +5,46 @@
   import MenuItem from '../../../Components/RightClickMenu/MenuItem.svelte';
   import MenuSeparator from '../../../Components/RightClickMenu/MenuSeparator.svelte';
   import { getFolderByID } from '../../../lib/api/get';
-  import MdCreateNewFolder from 'svelte-icons/md/MdCreateNewFolder.svelte';
+  import NewFolderIcon from 'svelte-icons/md/MdCreateNewFolder.svelte';
+  import UploadIcon from 'svelte-icons/md/MdCloudUpload.svelte';
+  import Overlay from '../../../Components/Overlays/Overlay.svelte';
+  import Upload from '../../../Components/Overlays/Upload.svelte';
+  import Delete from '../../../Components/Overlays/Delete.svelte';
+  import CreateFolder from '../../../Components/Overlays/CreateFolder.svelte';
 
   let path = '/';
   page.subscribe(async (p) => {
-    path = (await getFolderByID(parseInt(p.params.id))).path;
+    if (p.params.id) path = (await getFolderByID(parseInt(p.params.id))).path;
   });
+
+  let showUploadOverlay: () => void;
+  let closeUploadOverlay: () => void;
+
+  let showDeleteOverlay: () => void;
+  let closeDeleteOverlay: () => void;
+
+  let showNewFolderOverlay: () => void;
+  let closeNewFolderOverlay: () => void;
 </script>
 
 <FileView bind:path />
 <CustomMenu>
-  <MenuItem
-    text="hey"
-    on:click={() => {
-      console.log('hey');
-    }}
-  />
-  <MenuSeparator />
-  <MenuItem
-    text="loooooooooooooooooooong"
-    on:click={() => {
-      console.log('hey');
-    }}
-  />
-  <MenuItem
-    text="hey"
-    on:click={() => {
-      console.log('hey');
-    }}
-    disabled={true}
-  >
-    <MdCreateNewFolder slot="icon" />
+  <MenuItem text="New folder" on:click={showNewFolderOverlay}>
+    <NewFolderIcon slot="icon" />
+  </MenuItem>
+  <MenuItem text="Upload file" on:click={showUploadOverlay}>
+    <UploadIcon slot="icon" />
   </MenuItem>
 </CustomMenu>
+
+<Overlay bind:close={closeUploadOverlay} bind:open={showUploadOverlay}>
+  <Upload close={closeUploadOverlay} />
+</Overlay>
+
+<Overlay bind:close={closeDeleteOverlay} bind:open={showDeleteOverlay}>
+  <Delete close={closeDeleteOverlay} />
+</Overlay>
+
+<Overlay bind:close={closeNewFolderOverlay} bind:open={showNewFolderOverlay}>
+  <CreateFolder close={closeNewFolderOverlay} />
+</Overlay>
