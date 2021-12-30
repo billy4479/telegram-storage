402da7b9e7e2010f4680d047cb8113fd049e9c23
billy4479 <giachi.ellero@gmail.com>
2021-12-30 02:20:23 -0330

    Backend: add botapifm

--- backend/bot/getFileURL.go
+++ backend/bot/getFileURL.go
@@ -1,5 +1,36 @@
 package bot
 
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
 func (i *BotInterface) GetFileURL(fileID string) (string, error) {
-	return i.bot.GetFileDirectURL(fileID)
+	filePath, err := i.bot.GetFile(tgbotapi.FileConfig{FileID: fileID})
+	if err != nil {
+		return "", err
+	}
+
+	res, err := http.Post("http://bot-api:4479/download", "text/plain", strings.NewReader(filePath.FilePath))
+	if err != nil {
+		return "", err
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if res.StatusCode != 200 {
+		return "", errors.New(string(b))
+	}
+
+	url := fmt.Sprintf("http://bot-api:4479/download/%s", string(b))
+	fmt.Println(url)
+	return url, nil
 }
