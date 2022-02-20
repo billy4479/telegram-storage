ffd9cdad4d0a7e4810e8a1e0d5252e8d76cdf3df
billy4479 <giachi.ellero@gmail.com>
2022-02-20 19:13:05 -0330

    Frontend: Update landing page

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -19,19 +19,35 @@
 {#if !$isAuthenticatedStore}
   <h1 class="text-center text-6xl mt-10">Landing page</h1>
 
-  <div class="flex justify-center place-items-center flex-col">
-    <p class="mt-10 w-2/3 text-center">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nam
-      quod repudiandae consequuntur natus quo minus vitae voluptas temporibus
-      itaque voluptate assumenda ratione, quibusdam quam cumque dolorem nobis
-      aliquid omnis adipisci tempore veniam at? Officiis ad, delectus,
-      accusantium nam corporis voluptates iusto saepe molestiae velit ipsum
-      modi? Corporis, amet asperiores?
-    </p>
+  <div class="flex justify-center place-items-center flex-col ">
+    <div class="mt-10 w-2/3 text-center">
+      <p class="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nam
+        quod repudiandae consequuntur natus quo minus vitae voluptas temporibus
+        itaque voluptate assumenda ratione, quibusdam quam cumque dolorem nobis
+        aliquid omnis adipisci tempore veniam at? Officiis ad, delectus,
+        accusantium nam corporis voluptates iusto saepe molestiae velit ipsum
+        modi? Corporis, amet asperiores?
+      </p>
 
-    <div class="mt-10">
-      <a href="/login" class="btn">Login</a>
-      <a href="/register" class="btn btn-primary">Register</a>
+      <div class="divider" />
+
+      <p class="mt-5">
+        This page is still WIP...
+        <br />
+        You can check out the progress on
+        <a
+          href="https://github.com/billy4479/telegram-storage/"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="link link-primary">Github</a
+        >.
+      </p>
+
+      <div class="mt-10">
+        <a href="/login" class="btn">Login</a>
+        <a href="/register" class="btn btn-primary">Register</a>
+      </div>
     </div>
   </div>
 {/if}
