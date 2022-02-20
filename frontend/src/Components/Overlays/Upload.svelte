9a3d4e54893bd0b81b0043734c034bc1f1be1d5f
billy4479 <giachi.ellero@gmail.com>
2022-02-20 16:11:21 -0330

    Frontend: Port the rest of the overlays to the new UI

--- frontend/src/Components/Overlays/Upload.svelte
+++ frontend/src/Components/Overlays/Upload.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { refreshCurrentView } from '$lib/navigation';
   import { upload } from '$lib/api/upload';
+  import UploadIcon from 'svelte-icons/md/MdFileUpload.svelte';
 
   export let close: () => void;
 
@@ -11,7 +12,7 @@
     close();
   }
 
-  function onSubmit() {
+  function send() {
     upload(inputFiles.files).finally(refreshCurrentView);
     resetAndClose();
   }
@@ -19,12 +20,27 @@
 
 <h1 class="text-3xl mb-10">Upload</h1>
 
-<form on:submit|preventDefault={onSubmit}>
-  <input type="file" name="file" id="file" multiple bind:this={inputFiles} />
-  <br />
-  <div class="flex justify-evenly mt-10">
-    <input type="submit" value="Upload" class="btn-good cursor-pointer" />
-    <button class="btn-bad-light" on:click|preventDefault={close}>Cancel</button
+<form class="form-control">
+  <div class="flex justify-center">
+    <label class="label btn btn-accent px-3 gap-2" for="file">
+      <div class="w-7">
+        <UploadIcon />
+      </div>
+      <span class="text-lg">Browse</span>
+    </label>
+  </div>
+  <input
+    type="file"
+    name="file"
+    id="file"
+    class="hidden"
+    multiple
+    bind:this={inputFiles}
+    on:change={send}
+  />
+  <div class="modal-action">
+    <!-- <input type="submit" value="Upload" class="btn btn-primary" /> -->
+    <button class="btn btn-error" on:click|preventDefault={close}>Cancel</button
     >
   </div>
 </form>
